786325defaf70f10caa89c6f0277314747e2f2a8
Reyesalv20 <124184473+Reyesalv20@users.noreply.github.com>
2023-08-04 15:36:55 -0600

    inicio

---
+++ src/main/java/mx/com/gm/sudoku/Sudoku.java
@@ -0,0 +1,17 @@
+
+package mx.com.gm.sudoku;
+
+public class Sudoku {
+
+    public static void main(String[] args) {
+       
+       soduku obj=new soduku();
+       obj.CreateBoard();
+        
+        
+        
+        
+        
+        
+    }
+}
